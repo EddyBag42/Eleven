5d6bb0d226edf00ad55917bcc6984dd38f406668
EddyBag42 <edroidac@gmail.com>
2014-11-01 22:20:15 +0100

    Initial commit

---
+++ source/Eleven.ino
@@ -0,0 +1,150 @@
+#include <SPI.h>
+#include <Gamebuino.h>
+#include <math.h>
+#include <EEPROM.h>
+Gamebuino gb;
+
+// these two default fonts are already defined in the Gamebuino library
+// the "extern" modifier is used to be able to use a variable declared in another file
+extern const byte font3x5[]; //a small but efficient font (default)
+extern const byte font5x7[]; //a large, comfy font
+
+int line0[4] = {
+  0,0,0,0};
+int line1[4] = {
+  0,0,0,0};
+int line2[4] = {
+  0,0,0,0};
+int line3[4] = {
+  0,0,0,0};
+int* board[4] = {
+  line0,line1,line2,line3};
+
+int highScore=0;
+int score=0;
+int row[4];
+boolean notBlocked;
+int move;
+boolean won;
+
+
+///////////////////////////////////// SETUP
+void setup() {
+  int x1,x2,y1,y2;
+  gb.begin(); //initialize the Gamebuino
+  gb.display.setFont(font5x7);
+  gb.titleScreen(F("Eleven")); //shows the main menu
+  gb.pickRandomSeed();
+  gb.display.setFont(font3x5);
+  gb.battery.show = false;
+  x1=random(0,3);
+  x2=random(0,3);
+  y1=random(0,3);
+  y2=random(0,3);
+  while (x1==x2 && y1==y2){
+    x2=random(0,3);
+    y2=random(0,3);
+  }
+  board[x1][y1]=1;
+  board[x2][y2]=1;
+}
+
+///////////////////////////////////// LOOP
+void loop() {
+  if(gb.update()){
+    notBlocked=false;
+    move=0;
+    if(gb.buttons.pressed(BTN_C)){
+      gb.titleScreen(F("Eleven")); //shows the main menu
+    }
+    else if(gb.buttons.pressed(BTN_UP) && !won){
+      move=1; // up
+      for (int i=0; i<=3; i++){
+        // read the column i into row
+        for (int j=0; j<=3;j++){
+          row[j]=board[j][i];
+        }
+        // collapse row and count points
+        notBlocked=collapse(row,&score)||notBlocked;
+        // store row back to board
+        for (int j=0; j<=3;j++){
+          board[j][i]=row[j];
+        }
+      }
+    }
+    else if(gb.buttons.pressed(BTN_DOWN) && !won){
+      move=2; // down
+      for (int i=0; i<=3; i++){
+        // read the column i into row
+        for (int j=0; j<=3;j++){
+          row[j]=board[3-j][i];
+        }
+        // collapse row and count points
+        notBlocked=collapse(row,&score)||notBlocked;
+        // store row back to board
+        for (int j=0; j<=3;j++){
+          board[3-j][i]=row[j];
+        }
+      }
+    }
+    else if(gb.buttons.pressed(BTN_LEFT) && !won){
+      move=3; // left
+      for (int i=0; i<=3; i++){
+        // read the line i into row
+        for (int j=0; j<=3;j++){
+          row[j]=board[i][j];
+        }
+        // collapse row and count points
+        notBlocked=collapse(row,&score)||notBlocked;
+        // store row back to board
+        for (int j=0; j<=3;j++){
+          board[i][j]=row[j];
+        }
+      }
+    }
+    else if(gb.buttons.pressed(BTN_RIGHT) && !won){
+      move=4; // right
+      for (int i=0; i<=3; i++){
+        // read the line i into row
+        for (int j=0; j<=3;j++){
+          row[j]=board[i][3-j];
+        }
+        // collapse row and count points
+        notBlocked=collapse(row,&score)||notBlocked;
+        // store row back to board
+        for (int j=0; j<=3;j++){
+          board[i][3-j]=row[j];
+        }
+      }     
+    }
+    else if(gb.buttons.pressed(BTN_B)){
+      reset();
+    }
+    if (move!=0 && notBlocked && !won){
+      newone(move);
+    }
+    if (score>highScore){
+      highScore=score;
+    }
+    boardPrint();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ source/func.ino
@@ -0,0 +1,240 @@
+void reset(){
+  int x1,x2,y1,y2;
+  for (int i=0;i<=3;i++){
+    for (int j=0;j<=3;j++){
+      board[i][j]=0;
+    }
+  }
+  x1=random(0,3);
+  x2=random(0,3);
+  y1=random(0,3);
+  y2=random(0,3);
+  while (x1==x2 && y1==y2){
+    x2=random(0,3);
+    y2=random(0,3);
+  }
+  board[x1][y1]=1;
+  board[x2][y2]=1;
+  score=0;
+}
+
+boolean collapse(int row[4],int *score){
+  int points;
+  boolean hasmoved=false;
+  // move tiles together
+  hasmoved=removeBlanks(row);
+  // add tiles
+  points=add(row);
+  *score+=points;
+  return hasmoved || points!=0;
+}
+
+boolean removeBlanks(int row[4]){
+  int destination=0;
+  boolean move=false;
+  for (int i=0; i<=3;i++){
+    if (row[i]!=0){
+      if (destination!=i){
+        row[destination]=row[i];
+        row[i]=0;
+        move=true;
+      }
+      destination++;
+    }
+  }
+  return move;
+}
+
+int add(int row[4]){
+  int points=0;
+  if (row[0]==row[1] && row[0]!=0){
+    row[0]+=1;
+    points+=pow(2,row[0])+1;
+    if (row[2]==row[3] && row[2]!=0){
+      row[2]+=1;
+      points+=pow(2,row[2])+1;
+      row[1]=row[2];
+      row[2]=0;
+      row[3]=0;
+      return points;
+    }
+    row[1]=row[2];
+    row[2]=row[3];
+    row[3]=0;
+    return points;
+  }
+  if (row[1]==row[2] && row[1]!=0){
+    row[1]+=1;
+    points+=pow(2,row[1])+1;
+    row[2]=row[3];
+    row[3]=0;
+    return points; 
+  }
+  if (row[2]==row[3] && row[2]!=0){
+    row[2]+=1;
+    points+=pow(2,row[2])+1;
+    row[3]=0;
+  }
+  return points; 
+}
+
+
+void newone(int dir){
+  int i,j,k,r,newTile;
+  int n=0;
+  r=random(0,100);
+  if (r>=90) {
+    newTile=2;
+  } 
+  else {
+    newTile=1;
+  }
+  for (i=0;i<=3;i++){
+    for (j=0;j<=3;j++){
+      if (board[i][j]==0){
+        n+=1;
+      }
+    }
+  }
+  if (n!=0){
+    k=random(1,n+1);
+    n=0;
+    for (i=0;i<=3;i++){
+      for (j=0;j<=3;j++){
+        if (board[i][j]==0){
+          n+=1;
+          if (n==k){
+            board[i][j]=newTile;
+            return;
+          }
+        }
+      }
+    }
+  }
+}
+
+boolean gameNotOver(){
+  int score=0;
+  boolean notGameOver=false;
+  // up
+  for (int i=0; i<=3; i++){
+    // read the column i into row
+    for (int j=0; j<=3;j++){
+      row[j]=board[j][i];
+    }
+    // collapse row and count points
+    notGameOver=collapse(row,&score)||notGameOver;
+  }
+  // down
+  for (int i=0; i<=3; i++){
+    // read the column i into row
+    for (int j=0; j<=3;j++){
+      row[j]=board[3-j][i];
+    }
+    // collapse row and count points
+    notGameOver=collapse(row,&score)||notGameOver;
+  }
+  // left
+  for (int i=0; i<=3; i++){
+    // read the line i into row
+    for (int j=0; j<=3;j++){
+      row[j]=board[i][j];
+    }
+    // collapse row and count points
+    notGameOver=collapse(row,&score)||notGameOver;
+  }
+  // right
+  for (int i=0; i<=3; i++){
+    // read the line i into row
+    for (int j=0; j<=3;j++){
+      row[j]=board[i][3-j];
+    }
+    // collapse row and count points
+    notGameOver=collapse(row,&score)||notGameOver;
+  }
+  return notGameOver;
+}
+
+void boardPrint(){
+  gb.display.cursorX=47;
+  gb.display.cursorY=1;
+  gb.display.setFont(font5x7);
+  gb.display.print("Eleven");
+  gb.display.setFont(font3x5);
+  gb.display.cursorX=50;
+  gb.display.cursorY=10;
+  gb.display.print("Score :");
+  gb.display.cursorX=55;
+  gb.display.cursorY=17;
+  gb.display.print(score);
+  gb.display.cursorX=50;
+  gb.display.cursorY=24;
+  gb.display.print("Highest :");
+  gb.display.cursorX=55;
+  gb.display.cursorY=31;
+  gb.display.print(highScore);
+  if (!gameNotOver()){
+    gb.display.cursorX=47;
+    gb.display.cursorY=38;
+    gb.display.print("Game over");
+  }
+  for (int i=0;i<=3;i++){
+    for (int j=0;j<=3;j++){
+      gb.display.drawRect(j*11,i*11,12,12);
+      if (board[i][j]<=9){
+        gb.display.cursorX=4+j*11;
+        gb.display.cursorY=4+i*11;
+      }
+      else{
+        gb.display.cursorX=2+j*11;
+        gb.display.cursorY=4+i*11;
+      }
+      if (board[i][j]==11){
+        won=true;
+      }
+      if (board[i][j]!=0){
+        gb.display.print(board[i][j]);
+      }
+    }
+  }
+  if (won){
+    gb.display.setFont(font5x7);
+    gb.display.cursorX=47;
+    gb.display.cursorY=38;
+    gb.display.print("Won !");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
